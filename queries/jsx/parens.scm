19b29f5ef4f224de405b7d32fbd768f164374717
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-07-10 18:38:22 +0300

    [Fix](JsxSelfClosing): Added highlighting for self closing tag.

--- queries/jsx/parens.scm
+++ queries/jsx/parens.scm
@@ -1,13 +1,8 @@
 ; inherits: square,round,curly
 
-[(jsx_opening_element
-   name: (identifier) @left
-   attribute: (jsx_attribute (property_identifier) @left
-                              (string)))
-  (jsx-extended-rainbow-mode?)]
-
-;; "<" @left
-;; ">" @right
+[(jsx_self_closing_element
+   name: (identifier) @left)
+ (jsx-extended-rainbow-mode?)]
 
 [(jsx_opening_element
    name: (identifier) @left)
