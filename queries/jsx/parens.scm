51b40d708ab9029ac1fb7303574555934abba386
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2022-07-10 18:57:22 +0300

    [Enh](JsxNestedIdentifier): Added nested identifier to highlight.

--- queries/jsx/parens.scm
+++ queries/jsx/parens.scm
@@ -1,13 +1,13 @@
 ; inherits: square,round,curly
 
 [(jsx_self_closing_element
-   name: (identifier) @left)
+   name: [(identifier) (nested_identifier)] @left)
  (jsx-extended-rainbow-mode?)]
 
 [(jsx_opening_element
-   name: (identifier) @left)
+   name: [(identifier) (nested_identifier)] @left)
  (jsx-extended-rainbow-mode?)]
 
 [(jsx_closing_element
-   name: (identifier) @right)
+   name: [(identifier) (nested_identifier)] @right)
  (jsx-extended-rainbow-mode?)]
